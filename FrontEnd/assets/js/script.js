a243812af07c95284818e8d28cf92a7390837331
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2023-10-30 20:01:12 +0100

    Add function generateWorks

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -4,3 +4,24 @@ fetch("http://localhost:5678/api/works").then(function (reponse) {
     console.log(json)
     generateWorks(json);
 });
+
+function generateWorks(works){
+    for (let i = 0; i < works.length; i++) {
+
+        const work = works[i];
+
+        const sectionGallery = document.querySelector(".gallery");
+        const workElement = document.createElement("figure");
+
+        const imgElement = document.createElement("img");
+        imgElement.src = work.imageUrl;
+
+        const nomElement = document.createElement("figcaption");
+        nomElement.innerText = work.title;
+        
+        sectionGallery.appendChild(workElement);
+        workElement.appendChild(imgElement);
+        workElement.appendChild(nomElement);
+    
+     }
+}
